2ee49ca26a064c876e41c57ec8badf5719e1f56a
Laurence Elsdon <git@elsdon.me>
2023-11-11 10:56:12 +0000

    test(StudioLE.Verify): Added tests to check DiffTools.Resolved

---
+++ StudioLE.Verify/tests/Tools/DiffToolsTests.cs
@@ -0,0 +1,48 @@
+using DiffEngine;
+using NUnit.Framework;
+using StudioLE.Verify.NUnit;
+using StudioLE.Verify.Tests.Mock;
+
+namespace StudioLE.Verify.Tests.Tools;
+
+internal sealed class DiffToolsTests
+{
+    private readonly IVerify _verify = new NUnitVerify
+    {
+        UseAdditionalTools = false
+    };
+
+    [TestCase("BeyondCompare")]
+    [TestCase("Rider")]
+    [TestCase("VisualStudioCode")]
+    [Explicit("Requires tools to be installed")]
+    public void DiffTools_Resolved(string toolName)
+    {
+        // Arrange
+        // Act
+        ResolvedTool[] tools = DiffTools.Resolved.ToArray();
+        string[] toolNames = tools.Select(x => x.Name).ToArray();
+
+        // Assert
+        Assert.That(toolNames, Does.Contain(toolName));
+    }
+
+    [TestCase(DiffTool.BeyondCompare)]
+    [TestCase(DiffTool.Rider)]
+    [TestCase(DiffTool.VisualStudioCode)]
+    [Explicit("Intentionally failing")]
+    public async Task DiffTools_VisualTest(DiffTool tool)
+    {
+        // Arrange
+        const string actual = "THIS IS THE RECEIVED. The VERIFIED should be blank.";
+
+        // Act
+        DiffTools.UseOrder(tool);
+        ResolvedTool[] tools = DiffTools.Resolved.ToArray();
+        string[] toolNames = tools.Select(x => x.Name).ToArray();
+
+        // Assert
+        Assert.That(toolNames, Does.Contain(tool.ToString()));
+        await _verify.String(actual);
+    }
+}
